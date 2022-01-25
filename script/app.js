0f922f3f0e05d10d611da92653cb59b4f3ffa80b
Andrzej <andrzej.rakowski79@gmail.com>
2022-01-25 21:08:39 +0100

    acquiring form input field values and assigning variables

--- script/app.js
+++ script/app.js
@@ -1,1 +1,27 @@
+const formEl = document.querySelector("form");
+const containerEl = document.querySelector(".container");
+const noteElProto = document.querySelector(".prototype");
 
+const noteEl = noteElProto.cloneNode(true);
+
+console.log(noteEl.hasChildNodes());
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = e.target.elements.title.value;
+  const noteContent = e.target.elements.noteContent.value;
+  console.log(title, noteContent);
+  createNoteElement(title, noteContent);
+});
+
+const createNoteElement = function (title) {
+  const noteTitle = document.createElement("h1");
+  const noteContent = document.createElement("p");
+  const noteEl = document.createElement("div");
+  noteEl.classList.add("note");
+  noteTitle.innerText = title;
+  noteContent.innerText = noteContent;
+  noteEl.appendChild(noteTitle);
+  noteEl.appendChild(noteContent);
+  containerEl.appendChild(noteEl);
+};
